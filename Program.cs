ace5fdf2215541a29d516b89a6b7a2985bd7bc54
HenzoWTF <Urenahenzo@gmail.com>
2025-03-12 20:47:09 -0400

    Ultimo cambio de la version 1

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped<NotificationService>();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://www.apir.somee.com/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://www.apir.somee.com/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7279") });
 
 await builder.Build().RunAsync();
